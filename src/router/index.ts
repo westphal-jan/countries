7ebbdfd54cd51306f30366f3274c80a3533456c3
Jan Westphal <jan.westphal306@gmail.com>
2020-09-18 12:32:41 +0200

    Finish repo renaming

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,12 +6,12 @@ Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: "/flags",
+    path: "/countries",
     name: "Home",
     component: Home
   },
   {
-    path: "/flags/about",
+    path: "/countries/about",
     name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
